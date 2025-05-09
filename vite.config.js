31189b5e48462e0de9359934068a9de28fa992f1
Godwynnnn <Gabrielehodwyn@gmail.com>
2025-05-09 15:13:17 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path'; // ✅ Import path module
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/PortfolioReact/', // ✅ Needed for GitHub Pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // ✅ Alias '@' to 'src' folder
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
